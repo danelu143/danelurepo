f9016385f980a85c63d8eab0045311153bdacb0d
danelu143 <ydanelu@gmail.com>
2024-05-10 12:17:27 +0530

    Delete newsproject directory

--- newsproject/newsapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from newsapp.models import student
-class studentAdmin(admin.ModelAdmin):
-    list_display = ['sno','sname','mark']
-admin.site.register(student,studentAdmin)
--- newsproject/newsapp/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class student(models.Model):
-    sno=models.IntegerField()
-    sname=models.CharField(max_length=150)
-    mark=models.FloatField()
